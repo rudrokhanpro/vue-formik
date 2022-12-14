0ffacac7f09481bd926c9c019a3bc4f838ec0107
Rudro Khan <rudrokhan.pro@gmail.com>
2022-12-14 11:43:27 +0100

    add of initialValues, validate, errors, isSubmitting and handleSubmit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,51 @@
-<script setup lang="ts">
+<template>
+  <Formik :initial-values="initialValues" :validate="validate" @submit="handleSubmit" />
+</template>
+
+<script lang="ts">
 import Formik from "./components/Formik.vue"
+
+export default {
+  name: 'App',
+  components: {
+    Formik,
+  },
+  data() {
+    return {
+      initialValues: {
+        email: '',
+        password: '',
+      }
+    }
+  },
+  methods: {
+    validate(values) {
+      console.log(values)
+      const errors = {};
+
+      if (!values.email) {
+        errors.email = 'Required';
+      } else if (
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+      ) {
+        errors.email = 'Invalid email address';
+      }
+
+      if (!values.password) {
+        errors.password = 'Required';
+      }
+      return errors;
+    },
+    handleSubmit(values, setIsSubmitting) {
+      console.log('Form submitted', values)
+      setTimeout(() => {
+        setIsSubmitting(false);
+      }, 2000)
+    }
+  }
+}
 </script>
 
-<template>
-  <Formik />
-</template>
 
 <style scoped>
 
--- src/components/Formik.vue
+++ src/components/Formik.vue
@@ -1,5 +1,68 @@
 <template>
-  <form class="Formik">
+  <form class="Formik" @submit.prevent="handleSubmit">
     <h1>Formik</h1>
+    <pre>
+      Form: {{ JSON.stringify(form) }}
+      Errors: {{ JSON.stringify(errors) }}
+      Is submitting: {{ isSubmitting }}
+    </pre>
+    <div v-for="field in fields">
+      <input type="text" v-model="form[field]" :placeholder="field">
+      <p v-if="errors[field]">
+        {{ errors[field] }}
+      </p>
+    </div>
+    <button type="submit" :disabled="isSubmitting">Submit</button>
   </form>
 </template>
+
+<script lang="ts">
+const isEmpty = (obj: Object) => Object.keys(obj).length === 0
+
+export default {
+  name: 'Formik',
+  props: {
+    initialValues: {
+      type: Object,
+      required: true
+    },
+    validate: {
+      type: Function,
+      required: true,
+    },
+  },
+  emits: ['submit'],
+  data() {
+    return {
+      form: {},
+      errors: {},
+      isSubmitting: false,
+    }
+  },
+  computed: {
+    fields() {
+      const fields = Object.keys(this.initialValues)
+
+      return fields
+    },
+  },
+  methods: {
+    setIsSubmitting(isSubmitting) {
+      this.isSubmitting = isSubmitting
+    },
+    handleSubmit() {
+      const values = { ...this.form }
+      this.errors = this.validate(values)
+
+      if (isEmpty(this.errors)) {
+        this.$emit('submit', values, this.setIsSubmitting)
+
+        this.isSubmitting = true;
+      }
+    },
+  },
+  created() {
+    this.form = { ...this.initialValues };
+  }
+}
+</script>
